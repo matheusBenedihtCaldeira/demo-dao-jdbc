544b2e95d7994eb708e292413ace850dec8d2d4e
bene <matheusbenediht@gmail.com>
2023-07-16 17:20:58 -0300

    Implementando entidade de Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,15 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Matheus", "matheus@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
