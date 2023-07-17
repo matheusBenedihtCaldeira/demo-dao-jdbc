5988fb072cacb04db977d85951dd633e1572a45e
bene <matheusbenediht@gmail.com>
2023-07-17 19:15:42 -0300

    Insert Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import models.dao.SellerDao;
 import models.entities.Department;
 import models.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -28,5 +29,10 @@ public class Main {
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n==== TEST 4: Seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
