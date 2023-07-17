bd9597b9a4a6fd6595cbd518c7cb007b9fabb1fe
bene <matheusbenediht@gmail.com>
2023-07-17 19:02:44 -0300

    findAll

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,5 +22,11 @@ public class Main {
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n==== TEST 3: Seller findAll ====");
+        list = sellerDao.findAll();
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
